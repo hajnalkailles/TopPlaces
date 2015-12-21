a84975a7f1b7e541b3261ce012c8c45855d0a874
Hegyi <hajhegyi@hegyis-macbook-pro.local>
2015-12-21 16:26:16 +0100

    Initial Commit

---
+++ TopPlaces/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TopPlaces
+//
+//  Created by Hegyi Hajnalka on 21/12/15.
+//  Copyright © 2015 Hegyi Hajnalka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
