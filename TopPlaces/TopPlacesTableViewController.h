945889d9cb2fec5eb9a433d044d1603275286c3b
Hegyi <hajhegyi@Hegyis-MacBook-Pro.local>
2015-12-21 19:57:11 +0100

    Add FlickrFetcher helper class Add a TabBarViewController for top places and recently viewed images Get an array of top places

---
+++ TopPlaces/TopPlacesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopPlacesTableViewController.h
+//  TopPlaces
+//
+//  Created by Hegyi Hajnalka on 21/12/15.
+//  Copyright © 2015 Hegyi Hajnalka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopPlacesTableViewController : UITableViewController
+
+@end
